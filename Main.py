f6acaa7a4fe3ea9b73e46325b9e68d1fd2e866ae
Timothy Colledge <51029752+timmcoll03@users.noreply.github.com>
2021-09-09 10:34:51 -0400

    This is the first commit

---
+++ Main.py
@@ -0,0 +1,52 @@
+from turtle import *
+
+
+screen = Screen()
+screen.setup(1000, 1000)
+screen.bgcolor("red")
+screen.colormode(255)
+
+kingTurt = Turtle()
+kingTurt.shape("turtle")
+kingTurt.resizemode("auto")
+kingTurt.width(3)
+kingTurt.pencolor("blue")
+kingTurt.speed(0)
+#screen.tracer(0)
+
+def line(length, direction):
+    #kingTurt.setheading(direction)
+    kingTurt.left(direction)
+    kingTurt.forward(length)
+    return
+    
+
+
+
+def recurse(tik, baseSize, red, green, blue, xpos, ypos):
+    
+    print(tik)
+
+    kingTurt.pencolor(red, green, blue)
+    line(baseSize, 20)
+    
+    if tik <= 0:
+        return
+    else:
+        tik -= 1
+        recurse(tik, baseSize*.5, red, green+10, blue, )
+        recurse(tik, baseSize*.5, red, green+10, blue, )
+
+kingTurt.penup()
+kingTurt.setposition(0, -400)
+kingTurt.pendown()
+
+kingTurt.setheading(90)
+
+recurse(3, 100, 0, 0, 0, 0, -400)
+
+
+#screen.update()
+
+
+h=input("") 
