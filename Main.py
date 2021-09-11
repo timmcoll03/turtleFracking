59da24d31396d848ee90f15d69638ab5d5ab2a1f
Timothy Colledge <51029752+timmcoll03@users.noreply.github.com>
2021-09-11 18:32:08 -0400

    Near Done

--- Main.py
+++ Main.py
@@ -1,14 +1,30 @@
+# "Snowy Wonderland" Written by Timothy Colledge, Hunter Allen, and Kevin Cavicchia
+# CITE: Timothy Colledge
+# CITE: Hunter Allen
+# CITE: Kevin Cavicchia
+# Libraries Used:
+#   turtle - https://docs.python.org/3/library/turtle.html
+#   math - https://docs.python.org/3/library/math.html
+#   random - https://docs.python.org/3/library/random.html
+# Software Used
+#   Thonny 
+#   Visual Studio Code
+#   GitHub
+
+
+# Imports
 from turtle import *
 import random
 import math
-kick=10
 
 
+#Enviroment Initialization
 screen = Screen()
 screen.setup(1000, 1000)
 screen.bgcolor("#afdeee")
 screen.colormode(255)
 
+#Creating Turtle and Settings its Attributes
 kingTurt = Turtle()
 kingTurt.shape("turtle")
 kingTurt.resizemode("auto")
@@ -16,24 +32,27 @@ kingTurt.width(3)
 kingTurt.pencolor("blue")
 kingTurt.speed(0)
 screen.tracer(0)
+kingTurt.seth(90)
 
-        
-def stupid_line(length,angle):
+
+# Simple Function that Takes in lenght and goes forward that length and sends the turtle forward that direction
+# This Function does not do anything unique compared to forward but exists for further expandibility    
+def stupid_line(length):
     kingTurt.forward(length)
     return
 
+# This allows us to send the turtle somewhere based on x,y cordinates 
+# However, this also has built in pen features as compared to the turtle library goto() function
 def goto(x,y):
     kingTurt.penup()
     kingTurt.goto(x,y)
     kingTurt.pendown()
 
-
-kingTurt.seth(90)
+# tree fun uses recursion to draw a tree given imputs for the recusion count [kick] and length and angle of the tree branches
 def tree(kick,length,angle):    
-    if kick>=0:
-        
+    if kick>=0:       
         kick-=1
-        stupid_line(length,angle)
+        stupid_line(length)
         new_length=length-5
         kingTurt.left(angle)
         tree(kick, new_length,angle)
@@ -42,14 +61,28 @@ def tree(kick,length,angle):
         kingTurt.left(angle)
         kingTurt.backward(length)
         return
-    
+
+# This uses the tree function but in effect "spins" it, the imputs are the same as tree except the recursion 
+# size is always five the only thing we change is the size
+# and spindle count [which refrers to the number of "trees" used in one flake]    
 def snowflake(size,spindle, angle):
     for quant in range(spindle):
         kingTurt.seth((360/spindle)*quant)
         tree(5,size,angle)
         
+# Tree Creation 
+kingTurt.pensize(1)
+kingTurt.pencolor("brown")
+kingTurt.seth(90)
+goto(-300,-400)
+tree(6, 80, 20)
+goto(-175,-400)
+tree(5, 80, 20)
+goto(100,-400)
+tree(4, 80, 20)   
 
-   
+# Snow Creation
+kingTurt.pencolor("white")
 
 for snow in range (8):
     for snowy in range (30):
@@ -68,13 +101,16 @@ for line in range(1000):
     y = (3*math.sin(.04*x))-400
     kingTurt.pendown()
 
-kingTurt.seth(90)
-goto(0,-400)
 
-#tree(15, 80, 20)
+#Cloud Creation
+kingTurt.pencolor("grey")
+for cloud in range(12):
+    goto(-600+(100*cloud),550)
+    kingTurt.pensize(100)
+    kingTurt.circle(100,360)
+    kingTurt.pensize(1)
 
+#Cleanup Code and KillSwitch for running in IDEs other than Thonny
 screen.update()
 hideturtle()
-
-
 h=input("") 
